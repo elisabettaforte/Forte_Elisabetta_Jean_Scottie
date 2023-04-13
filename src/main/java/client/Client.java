e0c1a16004c7d078c3f1823cd48861b7b4829c06
elisabettaforte <128006581+elisabettaforte@users.noreply.github.com>
2023-04-13 17:08:02 -0400

    Creation du Client

---
+++ src/main/java/client/Client.java
@@ -0,0 +1,14 @@
+package client;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) {
+
+    }
+}
