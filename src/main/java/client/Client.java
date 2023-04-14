9274ee7e689ea22d6f243cdad525fad232951957
elisabettaforte <128006581+elisabettaforte@users.noreply.github.com>
2023-04-14 13:05:54 -0400

    Client

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,14 +1,32 @@
 package client;
 
-import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.net.ConnectException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
+        try{
+            Socket server = new Socket(int port)
+            System.out.println("Connecté au serveur");
 
+            // Envoyer une requête "charger" au serveur
+            PrintWriter pw = new PrintWriter(server.getOutputStream(), true);
+            pw.println("charger");
+
+            // Recuperer la liste des cours envoyee par le serveur
+            BufferedReader br = new BufferedReader(new InputStreamReader(server.getInputStream()));
+            String cours = br.readLine();
+            System.out.println(cours);
+
+            // Fermer les flux et la connexion
+            br.close();
+            pw.close();
+            server.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
