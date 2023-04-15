cda75f7e34003f95805fdbe1c2575c58869174b0
ScottieJean <scottie.jean@umontreal.ca>
2023-04-14 20:07:26 -0400

    Client.java

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,32 +1,84 @@
 package client;
 
-import java.io.BufferedReader;
+import javafx.util.Pair;
+
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
-    public static void main(String[] args) {
-        try{
-            Socket server = new Socket(int port);
-            System.out.println("Connecté au serveur");
 
-            // Envoyer une requête "charger" au serveur
-            PrintWriter pw = new PrintWriter(server.getOutputStream(), true);
-            pw.println("charger");
+    private final static String SERVER_ADDRESS = "localhost";
+    private final static int SERVER_PORT = 1337;
+    private final static String LOAD_COMMAND = "CHARGER";
+    private final static String REGISTER_COMMAND = "INSCRIRE";
 
-            // Recuperer la liste des cours envoyee par le serveur
-            BufferedReader br = new BufferedReader(new InputStreamReader(server.getInputStream()));
-            String cours = br.readLine();
-            System.out.println(cours);
+    private Socket socket;
+    private ObjectOutputStream objectOutputStream;
+    private ObjectInputStream objectInputStream;
 
-            // Fermer les flux et la connexion
-            br.close();
-            pw.close();
-            server.close();
+    public Client() {
+        try {
+            this.socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+            this.objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            this.objectInputStream = new ObjectInputStream(socket.getInputStream());
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Client démarré!");
+        while (true) {
+            System.out.print("Entrez une commande: ");
+            String input = scanner.nextLine();
+            Pair<String, String> parts = processCommandLine(input);
+            String cmd = parts.getKey();
+            String arg = parts.getValue();
+            try {
+                switch (cmd) {
+                    case LOAD_COMMAND:
+                        loadCourses();
+                        break;
+                    case REGISTER_COMMAND:
+                        registerCourse(arg);
+                        break;
+                    default:
+                        System.out.println("Commande non reconnue.");
+                }
+            } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void loadCourses() throws IOException, ClassNotFoundException {
+        this.objectOutputStream.writeObject(LOAD_COMMAND);
+        String response = (String) this.objectInputStream.readObject();
+        System.out.println(response);
+    }
+
+    private void registerCourse(String arg) throws IOException, ClassNotFoundException {
+        this.objectOutputStream.writeObject(REGISTER_COMMAND + " " + arg);
+        String response = (String) this.objectInputStream.readObject();
+        System.out.println(response);
+    }
+
+    private Pair<String, String> processCommandLine(String line) {
+        String[] parts = line.split(" ");
+        String cmd = parts[0];
+        String arg = "";
+        if (parts.length > 1) {
+            arg = String.join(" ", parts).substring(parts[0].length() + 1);
+        }
+        return new Pair<>(cmd, arg);
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
     }
 }
--- src/main/java/server/models/Course.java
+++ src/main/java/server/models/Course.java
@@ -1,6 +1,7 @@
 package server.models;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Course implements Serializable {
@@ -8,15 +9,27 @@ public class Course implements Serializable {
     private String name;
     private String code;
     private String session;
+    private static List<Course> allCourses = new ArrayList<>();
 
     public Course(String name, String code, String session) {
         this.name = name;
         this.code = code;
         this.session = session;
+        allCourses.add(this);
+
     }
 
-    public static List<Course> filterBySession(String session) {
+    public List<Course> filterBySession(String session) {
+        List<Course> filteredCourses = new ArrayList<>();
+        for (Course course : allCourses) {
+            if (course.getSession().equals(session)) {
+                filteredCourses.add(course);
+            }
+        }
+        return filteredCourses;
     }
+
+
 
     public String getName() {
         return name;
